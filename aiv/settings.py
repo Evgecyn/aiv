f72078c915764d18a375275a38320cf63fe0331f
alexander.ivanov <alexander.ivanov@First>
2009-12-21 22:43:42 +0500

    Подключил django-config

--- aiv/settings.py
+++ aiv/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.admin',
+    'config',
 )
 
 TEMPLATE_LOADERS = (
@@ -85,3 +86,6 @@ FORCE_SCRIPT_NAME = ''
 
 CACHE_BACKEND = 'locmem:///?max_entries=5000'
 
+# django-config settings
+CONFIG_SITES = ['www.a-iv.info', ]
+CONFIG_REDIRECTS = ['a-iv.info', ]
